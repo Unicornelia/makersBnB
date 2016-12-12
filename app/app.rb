302595cd032d81603cbf9cab5867e9392033f388
George Eaton <george_eaton@hotmail.co.uk>
2016-12-12 13:06:18 +0000

    adds new space form

--- app/app.rb
+++ app/app.rb
@@ -9,6 +9,14 @@ class MakersBnB < Sinatra::Base
     erb :'/spaces/new'
   end
 
+  post '/spaces/new' do
+    redirect('/spaces')
+  end
+
+  get '/spaces' do
+    erb :'/spaces/list_of_spaces'
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- spec/features/adding_spaces_spec.rb
+++ spec/features/adding_spaces_spec.rb
@@ -1,9 +1,18 @@
 RSpec.feature 'adding a new space' do
-
   scenario 'a user can fill in their details when adding a new space' do
     visit '/spaces/new'
     expect(page.status_code).to eq (200)
     expect(page).to have_content ('Add your space to rent below!')
+  end
+
+  scenario 'a user can add a new space' do
+    visit '/spaces/new'
+    fill_in 'space_name', with: 'My space'
+    fill_in 'space_description', with: "It's very nice."
+    fill_in 'price', with: '50'
+    click_button 'Add space'
+    expect(current_path).to eq('/spaces')
+    expect(page).to have_content('Your space has been added!')
   end
 
 
