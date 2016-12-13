757ee4995c3ddb7bcde2eb581e8f6001dbc37408
unicornelia <ez.kornelia@gmail.com>
2016-12-13 16:24:39 +0000

    Added date availablity method for space class, tests are working

--- app/models/space.rb
+++ app/models/space.rb
@@ -8,6 +8,13 @@ class Space
   property :space_name, String
   property :space_description, Text
   property :price, String
+  property :start_date, Date
+  property :end_date, Date
+
+  def available?(requested_date)
+    return true if requested_date >= self.start_date && requested_date < self.end_date
+    false
+end
 
 end
 
