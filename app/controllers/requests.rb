ab73dc2741960b8523863d0112a0760464f5a19a
George Eaton <george_eaton@hotmail.co.uk>
2016-12-14 16:47:03 +0000

    requests can be created, but not sent to owner yet

--- app/controllers/requests.rb
+++ app/controllers/requests.rb
@@ -1,7 +1,18 @@
+require_relative '../models/request'
+
 class MakersBnB < Sinatra::Base
 
   get '/requests/manager' do
     erb :'requests/manager'
   end
-  
+
+  post '/requests' do
+  request = current_user.requests.new(requested_date: '02/12/2016', confirmed: false, space_id: params[:space_id])
+    if request.save
+      flash.next[:request_sent] = "Booking request sent!"
+    else
+      redirect('/requests/manager')
+    end
+  end
+
 end
