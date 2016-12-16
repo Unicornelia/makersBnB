2eb856fcb667d9adf6d8926a6fafc51ae3a3cf38
George Eaton <george_eaton@hotmail.co.uk>
2016-12-16 10:54:46 +0000

    bookings can be confirmed

--- spec/features/confirming_requests_spec.rb
+++ spec/features/confirming_requests_spec.rb
@@ -3,17 +3,18 @@ describe "confirming requests" do
   scenario 'before a booking is confirmed, booking request status is "awaiting confirmation"' do
     book_a_space
     click_link 'View requests'
-    expect(page).to have_content "Booking status: false"
+    expect(page).to have_content "Booking status: Awaiting confirmation"
   end
 
-  scenario 'accepting a booking sets the booking request status to "confirmed"' do
+  scenario 'accepting a booking removes confirm button and sets the booking request status to "confirmed"' do
     confirm_booking
+    expect(page).not_to have_content("Confirm booking")
     click_button('Log Out')
     visit "/sessions/new"
     fill_in "username", :with => "Chrixs"
     fill_in "password", :with => "1234"
-    click_button "Log In" 
+    click_button "Log In"
     click_link('View requests')
-    expect(page).to have_content "Booking status: true"
+    expect(page).to have_content "Booking status: Confirmed!"
   end
 end
